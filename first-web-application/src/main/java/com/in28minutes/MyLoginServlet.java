ccaa13388e44b421787c3899320e8d4fb1277f0d
Rajasekhar Karanam <karanam.rajasekhar44@gmail.com>
2018-11-04 12:35:27 +0530

    Servlets & Jsps Course: Basic JSP

---
+++ first-web-application/src/main/java/com/in28minutes/MyLoginServlet.java
@@ -0,0 +1,24 @@
+package com.in28minutes;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet(urlPatterns = "/mylogin.do")
+public class MyLoginServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+	
+	  protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+			  throws ServletException, IOException {
+		  
+		  request.getRequestDispatcher("/WEB-INF/views/mylogin.jsp").forward(request, response);
+		  
+	  }	
+
+}
